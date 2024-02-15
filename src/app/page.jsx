006c2b628c2a00c8917816b02895371b0434c3f1
Souptik Dam <sddd8043@gmail.com>
2024-02-15 18:19:58 +0530

    initialised

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import "./index.css"
+import { Table } from "./components/Table";
+
+
+
+function App() {
+    return (        
+        <div className="table-container">
+            <Table />
+        </div>
+    )
+}
+
+export default App;
